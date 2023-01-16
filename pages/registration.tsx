7aedbdc78d97ecd76c68dbfef2915e2ba2493829
Dmitry Karpekin <dkarpekin@twelvedevs.com>
2023-01-16 19:49:22 +0300

    added error message

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,14 +1,19 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useFormik } from "formik";
 import config from "config";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import ReCAPTCHA from "react-google-recaptcha";
-import { createRef } from "react";
+import { createRef, useState } from "react";
 
 type PropsType = { appUrl: string; recaptchaKey: string };
 
+type ErrorResponse = {
+  message: string;
+};
+
 export default function Registration({ appUrl, recaptchaKey }: PropsType) {
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const recaptchaRef = createRef<ReCAPTCHA>();
   const router = useRouter();
   const formik = useFormik({
@@ -20,27 +25,36 @@ export default function Registration({ appUrl, recaptchaKey }: PropsType) {
     },
     onSubmit: async (values) => {
       if (values.password !== values.repeatPassword) {
-        alert("repeatPassword");
+        setErrorMessage("repeatPassword");
         return;
       }
       const recaptchaValue = recaptchaRef.current?.getValue();
       if (!recaptchaValue) {
+        setErrorMessage("no recaptchaValue");
         return;
       }
 
       values.recaptchaValue = recaptchaValue;
 
-      const req = await axios.post(`${appUrl}/api/users/registration`, values);
-      if (req.status === 200) {
-        router.push("/signin");
-      } else {
-        /**/
+      try {
+        const req = await axios.post(
+          `${appUrl}/api/users/registration`,
+          values
+        );
+        if (req.status === 200) {
+          router.push("/signin");
+        }
+      } catch (err) {
+        const error = err as AxiosError;
+        const response = error.response;
+        setErrorMessage((response?.data as ErrorResponse).message);
       }
     },
   });
 
   return (
     <>
+      {errorMessage ? <div>{errorMessage}</div> : <></>}
       <form onSubmit={formik.handleSubmit}>
         <input
           name="email"
