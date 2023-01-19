4222c7d99061058717620f72e9f658cf29f22c61
Talerchik-Aleksey <talerchik@protonmail.com>
2023-01-19 13:37:07 +0300

    Delete console.log

--- utils/eventTimeStatus.ts
+++ utils/eventTimeStatus.ts
@@ -1,6 +1,5 @@
 export function isToday(date: Date): boolean {
   const today = new Date();
-  console.log(today);
 
   if (today.toDateString() === date.toDateString()) {
     return true;
@@ -11,7 +10,6 @@ export function isToday(date: Date): boolean {
 export function isTomorrow(date: Date): boolean {
   const today = new Date();
   const tomorrow = new Date(today.setDate(today.getDate() + 1));
-  console.log(tomorrow);
 
   if (tomorrow.toDateString() === date.toDateString()) {
     return true;
