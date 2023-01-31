34590e8fe0a2bfacf6a69dcf9d2824bc8a94ce00
Anastasya Boltach <aboltach@twelvedevs.com>
2023-01-31 16:16:51 +0300

    fix terms

--- Components/General/Terms/Terms.tsx
+++ Components/General/Terms/Terms.tsx
@@ -1,33 +1,59 @@
-import styles from "./Textbox.module.scss";
+import styles from "./Terms.module.scss";
 
-export default function Textbox() {
+export default function Terms() {
   return (
     <section className={styles.container}>
-      <div className={styles.textbox}>
-        <h4 className={styles.textboxTitle}>What Is LibertyMeets?</h4>
-        <p className={styles.textboxDescription}>
-          Unlike social media sites, LibertyMeets is geared toward in-person
-          connections to form local bonds and take action in your community.
-          Explore and post opportunities to find other freedom-oriented
-          individuals, businesses, non-profits, and campaigns.
+      <div className={styles.terms}>
+        <h4 className={styles.termsTitle}>Our Privacy Policy</h4>
+        <p className={styles.termsDescriptionBold}>
+          This Privacy Policy describes Our policies and procedures on the
+          collection, use and disclosure of Your information when You use the
+          Service and tells You about Your privacy rights and how the law
+          protects You. We use Your Personal data to provide and improve the
+          Service. By using the Service, You agree to the collection and use of
+          information in accordance with this Privacy Policy.
+          <br />
+          <br />
+          This privacy
+          policy applies to the information that You provide to LibertyMeets.com
+          (“Service”).
         </p>
       </div>
       <div className={styles.textbox}>
-        <h4 className={styles.textboxTitle}>Your Privacy Is Paramount</h4>
-        <p className={styles.textboxDescription}>
-          Sign up information is to ensure users are connecting with real
-          people. Other than that, your business isn’t our business. We do not
-          and will not share or sell your information. When someone replies to
-          your post, or you reply to a post, your email address is anonymous.
+        <h4 className={styles.termsTitle}>What Info We Collect</h4>
+        <p className={styles.termsDescription}>
+          We may collect, use, transfer, and disclose Your information for the following purposes:
+          <ul className={styles.termsList}>
+            <li>to facilitate and perform services You have requested;</li>
+            <li>to provide You with information that You request or which You may need in order to contact Us (for example, if You submit a comment, question or complaint to Us, We may respond to Your message);</li>    <li>for internal recordkeeping;</li>
+            <li>to administer the Service;</li>
+            <li>to communicate with You regarding the Service;</li>
+            <li>to improve and customize the Service;</li>
+            <li>to notify You about changes to the Service;</li>
+            <li>to notify You about changes to the Service;  </li>
+            <li>to comply with the law; and </li>
+            <li>as described to You at the point where You provide Your information to Us.</li>
+          </ul>
         </p>
       </div>
       <div className={styles.textbox}>
-        <h4 className={styles.textboxTitle}>How Does It Work?</h4>
-        <p className={styles.textboxDescription}>
-          Simplicity and speed are our goals. Anyone can browse and respond to
-          public posts. Sign up to post, view and respond to private posts, and
-          keep track of your favorite posts. Heres a short video exploring the
-          sites functionality.
+        <h4 className={styles.termsTitle}>How Does It Work?</h4>
+        <p className={styles.termsDescription}>
+          We use Your information to provide and improve the
+           Service. We do not share Your information with third 
+           parties.</p>
+          <span className={styles.cookies}>COLLECTION AND USE OF COOKIES</span>
+          <p className={styles.termsDescription}>
+          A cookie is a small piece of data stored 
+          on Your computer that identifies Your browser 
+          and thatThis Privacy Policy describes Our policies 
+          and procedures on the collection, use and disclosure 
+          of Your information when You use the Service and tells
+          You about Your privacy rights and how the law protects
+          You. We use Your Personal data to provide and improve 
+          the Service. By using the Service, You agree to the
+          collection and use of information in accordance with
+          this Privacy Policy.
         </p>
       </div>
     </section>
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -1,49 +1,7 @@
-export default function Terms() {
+import Terms from "../Components/General/Terms/Terms";
+
+export default function privacy() {
   return (
-    <>
-      <div>
-        <div>
-          <h1>Our Privacy Policy</h1>
-          This Privacy Policy describes Our policies and procedures on the
-          collection, use and disclosure of Your information when You use the
-          Service and tells You about Your privacy rights and how the law
-          protects You. We use Your Personal data to provide and improve the
-          Service. By using the Service, You agree to the collection and use of
-          information in accordance with this Privacy Policy. This privacy
-          policy applies to the information that You provide to LibertyMeets.com
-          (“Service”).
-        </div>
-        <div>
-          <h1>What Info We Collect</h1>
-          <p>
-            We may collect, use, transfer, and disclose Your information for the
-            following purposes: to facilitate and perform services You have
-            requested; to provide You with information that You request or which
-            You may need in order to contact Us (for example, if You submit a
-            comment, question or complaint to Us, We may respond to Your
-            message); for internal recordkeeping; to administer the Service; to
-            communicate with You regarding the Service; to improve and customize
-            the Service; to notify You about changes to the Service; to comply
-            with the law; and as described to You at the point where You provide
-            Your information to Us.
-          </p>
-        </div>
-        <div>
-          <h1>How We Use That Info</h1>
-          <p>
-            We use Your information to provide and improve the Service. We do
-            not share Your information with third parties. COLLECTION AND USE OF
-            COOKIES A cookie is a small piece of data stored on Your computer
-            that identifies Your browser and thatThis Privacy Policy describes
-            Our policies and procedures on the collection, use and disclosure of
-            Your information when You use the Service and tells You about Your
-            privacy rights and how the law protects You. We use Your Personal
-            data to provide and improve the Service. By using the Service, You
-            agree to the collection and use of information in accordance with
-            this Privacy Policy.
-          </p>
-        </div>
-      </div>
-    </>
+  <Terms/>
   );
 }
