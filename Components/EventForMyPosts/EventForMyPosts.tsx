d06e1e7a5b33c27f84675d12e3405722d8198c26
Sh1NaiK <115789053+Sh1NaiK@users.noreply.github.com>
2023-02-01 16:29:08 +0300

    fix

--- Components/EventForMyPosts/EventForMyPosts.tsx
+++ Components/EventForMyPosts/EventForMyPosts.tsx
@@ -33,11 +33,10 @@ export default function EventForMyPosts({
             {moment(post.created_at).format("MMM D, YYYY")}
           </div>
           <div className={styles.label}>{post.category}</div>
-          <div className={styles.info}>{post.description}</div>
-          
+          <div className={styles.info}>{post.title}</div>
         </Link>
-        
-      </div><div className={styles.location}>{post.geo}</div>
+      </div>
+      <div className={styles.location}>{post.geo}</div>
       {/* <div className={styles.location}>{post.geo}</div> */}
     </div>
   );
