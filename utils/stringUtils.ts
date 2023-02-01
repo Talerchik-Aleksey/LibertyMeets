8ef2e5d2a882d0f16dabd491cd66b470aac0f8c8
Sh1NaiK <115789053+Sh1NaiK@users.noreply.github.com>
2023-02-01 17:19:29 +0300

    fix after review

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -1,18 +1,5 @@
+import * as EmailValidator from "email-validator";
+
 export function validateEmail(email: string): boolean {
-  // const start = /^/;
-  // const username = /([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+")/;
-  // const at = /@/;
-  // const domain =
-  //   /((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
-  // const end = /$/;
-
-  // const regex = new RegExp(
-  //   `${start.source}${username.source}${at.source}${domain.source}${end.source}`
-  // );
-
-  // return regex.test(email.toLocaleLowerCase());
-
-  const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
-  return expression.test(email);
+  return EmailValidator.validate(email);
 }
