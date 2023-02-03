a72f1af56f35172c1c8a4880df2b787a4b8a6592
Talerchik-Aleksey <talerchik@protonmail.com>
2023-02-03 13:53:52 +0300

    print password reset status

--- Components/General/CheckEmail/CheckEmail.tsx
+++ Components/General/CheckEmail/CheckEmail.tsx
@@ -4,7 +4,10 @@ export default function CheckEmail() {
   return (
     <section className={styles.container}>
       <h5 className={styles.message}>
-        Thank you, please check your email and follow the instructions.
+        Your password reset request has been successfully submitted! Please
+        check your email for further instructions on how to reset your password.
+        If you do not receive an email within the next hour, please check your
+        spam folder or contact support for assistance.
       </h5>
     </section>
   );
--- Components/ResetPassword/ResetPassword.tsx
+++ Components/ResetPassword/ResetPassword.tsx
@@ -5,12 +5,12 @@ import { useRouter } from "next/router";
 import styles from "./ResetPassword.module.scss";
 import Link from "next/link";
 import axios, { AxiosError } from "axios";
+import ResetPasswodTextStatus from "../../pages/reset-password/reset-password-text-status";
 
 type ResetPasswordProps = { appUrl: string };
 
 export default function ResetPassword({ appUrl }: ResetPasswordProps) {
-  const [isWrongEmail, setIsWrongEmail] = useState<boolean>(false);
-  const [generateLink, setGenerateLink] = useState<string>("");
+  const [resetEmailStatus, setResetEmailStatus] = useState<number>(200);
   const router = useRouter();
 
   async function onFinish(values: any) {
@@ -19,19 +19,16 @@ export default function ResetPassword({ appUrl }: ResetPasswordProps) {
         `${appUrl}/api/users/reset-password`,
         values
       );
+
       if (req.status === 200) {
-        setGenerateLink(`${appUrl}/reset-password/verify/${req.data.token}`);
         router.push("/reset-password/check-email");
       }
     } catch (err) {
       const error = err as AxiosError;
       const response = error.response;
-      if (response?.status === 403) {
-        setIsWrongEmail(true);
-      }
+      setResetEmailStatus(response?.status || 500);
     }
   }
-  console.log("generateLink <-------", generateLink);
 
   return (
     <section className={styles.resetPasswordWrapper}>
@@ -98,11 +95,12 @@ export default function ResetPassword({ appUrl }: ResetPasswordProps) {
               <span className={styles.resetText}>Reset Password</span>
             </Button>
           </Form.Item>
+          <ResetPasswodTextStatus errCode={resetEmailStatus} />
         </Form>
         <Link className={styles.goBack} href={"/signin"}>
           Go Back to Log In
         </Link>
-      </div>    
+      </div>
     </section>
   );
 }
