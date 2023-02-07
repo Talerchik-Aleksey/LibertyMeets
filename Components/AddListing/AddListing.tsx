0011843446e5c5bf2934aae7aa69e7d7bdcda916
Talerchik-Aleksey <talerchik@protonmail.com>
2023-02-07 15:59:08 +0300

    AddListing session check

--- Components/AddListing/AddListing.tsx
+++ Components/AddListing/AddListing.tsx
@@ -1,26 +1,29 @@
-import { Button } from "antd";
 import styles from "./AddListing.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function AddListing() {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
       <div className={styles.noCriteria}>
         No Further Opportunities Meet This Criteria
       </div>
       <div className={styles.title}>Add Your Listing</div>
-      <Link className={styles.createPost} href={"/createPost"}>
-        
-          <Image
-            src="/decor/Vector3.svg"
-            alt=""
-            width={16}
-            height={14}
-            className={styles.vector}
-          />
-          <span className={styles.buttonText}>Create Post</span>
-      
+      <Link
+        className={styles.createPost}
+        href={session.status === "authenticated" ? "/createPost" : "/signin"}
+      >
+        <Image
+          src="/decor/Vector3.svg"
+          alt=""
+          width={16}
+          height={14}
+          className={styles.vector}
+        />
+        <span className={styles.buttonText}>Create Post</span>
       </Link>
     </div>
   );
