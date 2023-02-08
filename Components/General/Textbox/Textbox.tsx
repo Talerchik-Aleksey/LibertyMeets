2c5c46f52dd82f61801dedcaef5e04091b98fec0
Anastasya Boltach <aboltach@twelvedevs.com>
2023-02-08 17:29:17 +0300

    fix textbox

--- Components/General/Textbox/Textbox.tsx
+++ Components/General/Textbox/Textbox.tsx
@@ -5,30 +5,54 @@ export default function Textbox() {
     <section className={styles.container}>
       <div className={styles.textbox}>
         <h4 className={styles.textboxTitle}>What Is LibertyMeets?</h4>
-        <p className={styles.textboxDescription}>
-          Unlike social media sites, LibertyMeets is geared toward in-person
-          connections to form local bonds and take action in your community.
-          Explore and post opportunities to find other freedom-oriented
-          individuals, businesses, non-profits, and campaigns.
-        </p>
+        <ul className={styles.textboxDescription}>
+          <li>Unlike social media sites, LibertyMeets
+            is geared toward in-person connections to
+            form local bonds and take action in your community.
+          </li>
+          <li>Many conservative/libertarians/moderates
+            of all ages would like to get more involved
+            politically but don't know exactly how,
+            beyond contacting representatives/campaigns,
+            searching in a roundabout way where/how to volunteer,
+            responding to solicitations, posting on social media,
+            and voting.
+          </li>
+          <li>They would also like to find new like-minded
+            peers to engage with socially and professionally,
+            especially younger adults who may feel isolated in
+            progressive cities and organizations. Their fervor
+            is being undeserved without a clearer and easier way
+            to connect in-person with other freedom-oriented people.
+          </li>
+        </ul>
       </div>
       <div className={styles.textbox}>
         <h4 className={styles.textboxTitle}>Your Privacy Is Paramount</h4>
-        <p className={styles.textboxDescription}>
-          Sign up information is to ensure users are connecting with real
-          people. Other than that, your business isn’t our business. We do not
-          and will not share or sell your information. When someone replies to
-          your post, or you reply to a post, your email address is anonymous.
-        </p>
+        <ul className={styles.textboxDescription}>
+          <li>Sign-up information is to ensure users
+            are connecting with real people.
+          </li>
+          <li>Other than that, your business
+            isn't our business. We do not and will not
+            share or sell your information.
+          </li>
+          <li>When you reply to a post, or someone replies
+            to your post, your email address is anonymous.
+            Feel free to see our privacy policy for details.
+          </li>
+        </ul>
       </div>
       <div className={styles.textbox}>
         <h4 className={styles.textboxTitle}>How Does It Work?</h4>
-        <p className={styles.textboxDescription}>
-          Simplicity and speed are our goals. Anyone can browse and respond to
-          public posts. Sign up to post, view and respond to private posts, and
-          keep track of your favorite posts. Heres a short video exploring the
-          sites functionality.
-        </p>
+        <ul className={styles.textboxDescription}>
+          <li>Simplicity and speed are our goals.</li>
+          <li>Anyone can browse and respond to public posts.</li>
+          <li>Sign up to post, view and respond to private posts,
+            and keep track of your favorite posts.
+          </li>
+          <li>Here's a short video exploring the site's functionality.</li>
+        </ul>
       </div>
     </section>
   );
