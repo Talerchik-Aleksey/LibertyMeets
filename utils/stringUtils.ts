02e46eef4ed5833a03920bb6eb387fbc6a1582a0
Talerchik-Aleksey <talerchik@protonmail.com>
2023-02-10 10:43:32 +0300

    New regex for password and tests

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -3,8 +3,7 @@ import * as EmailValidator from "email-validator";
 export function validateEmail(email: string): boolean {
   return EmailValidator.validate(email);
 }
+const pattern =
+  /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*\`\~\+\=])[A-Za-z\d!@#\$%\^&\*\`\~\+\=]{8,}/;
 
-export const PASSWORD_VALIDATION_PATTERN = new RegExp(
-  // "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
-  "^.{8,}$"
-);
+export const PASSWORD_VALIDATION_PATTERN = new RegExp(pattern);
