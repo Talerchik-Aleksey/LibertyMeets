040934e983a3977d71034e113f44cb6befe411b3
Talerchik-Aleksey <talerchik@protonmail.com>
2023-02-13 08:50:03 +0300

    Cleanup activation email body

--- Components/General/Pagination/Pagination.tsx
+++ Components/General/Pagination/Pagination.tsx
@@ -52,7 +52,7 @@ export function PaginationForPosts({
                   backgroundColor: "#921A64",
                   borderRadius: "50%",
                   color: "#ffffff",
-                  fontSize: "14px",
+                  fontSize: "14xt",
                 }}
               >
                 {element}
