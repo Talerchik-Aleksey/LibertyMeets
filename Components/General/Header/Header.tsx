785a335601b8604b602fa36d886e11376d7c4f6c
Dmitry Karpekin <dkarpekin@twelvedevs.com>
2023-02-13 19:19:26 +0300

    added links by header for 404 page

--- Components/General/Header/Header.tsx
+++ Components/General/Header/Header.tsx
@@ -14,7 +14,7 @@ import SignUp from "./buttons/signUp";
 import Image from "next/image";
 
 const buttonMap = {
-  showSearch: ["", "my-favorites", "myPosts", "settings", "about"],
+  showSearch: ["", "my-favorites", "myPosts", "settings", "about", "404"],
   showCreatePost: ["", "posts", "my-favorites", "myPosts", "settings", "about"],
   showMyProfile: [
     "",
@@ -26,6 +26,7 @@ const buttonMap = {
     "about",
     "posts/[postId]",
     "posts/edit/[postId]",
+    "404",
   ],
   showLogOut: [
     "",
@@ -45,6 +46,7 @@ const buttonMap = {
     "posts",
     "reset-password",
     "posts/[postId]",
+    "404",
   ],
   showSignUp: ["signin", "about", "posts", "reset-password", "posts/[postId]"],
 };
