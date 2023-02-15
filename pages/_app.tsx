88aacc264f81a34a1dfd70504dd4958ca1a7b1b7
Vadim Burdylev <vburdylev@twelvedevs.com>
2023-02-15 15:23:42 +0300

    add mount script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,29 @@ import type { AppProps } from "next/app";
 import Footer from "../Components/General/Footer/Footer";
 import { SessionProvider } from "next-auth/react";
 import Header from "../Components/General/Header/Header";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.innerHTML = `(function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:3366804,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <SessionProvider>
