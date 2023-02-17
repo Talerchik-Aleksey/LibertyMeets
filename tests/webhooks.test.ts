72317c86ec5a0f7f0f70a8f6d51913ea0254708c
Vadim Burdylev <vburdylev@twelvedevs.com>
2023-02-17 09:38:26 +0300

    add test for several tags

--- tests/webhooks.test.ts
+++ tests/webhooks.test.ts
@@ -1,5 +1,4 @@
 import addressparser from "addressparser";
-import { removeTagsFromEmail } from "../utils/stringUtils";
 
 describe("addressparser", () => {
   it("can parse", () => {
@@ -9,19 +8,5 @@ describe("addressparser", () => {
     expect(a.length).toBe(1);
     expect(a[0].address).toBe("vburdylev@twelvedevs.com");
     expect(a[0].name).toBe("Vadim Burdylev");
-  });
-});
-
-describe("removing tags", () => {
-  it("removes special tags from email", () => {
-    const email = "auto+remove_123@example.com";
-    expect(removeTagsFromEmail(email)).toBe("auto@example.com");
-  });
-
-  it("throws an error when incorrect parameters used", () => {
-    const email = "auto+remove_123_example.com";
-    expect(() => removeTagsFromEmail(email)).toThrow(
-      "Incorrect email format provided"
-    );
   });
 });
