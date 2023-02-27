fe8f424f39ad3046c0c020931de889ffde129f58
Andrey Pankov <a.pankov@gmail.com>
2023-02-27 18:09:05 +0300

    fix: send response immediately

--- pages/api/threads/mail-receiver.ts
+++ pages/api/threads/mail-receiver.ts
@@ -4,24 +4,24 @@ import { handleWebhook } from "../../../services/webhook";
 import { errorResponse } from "../../../utils/response";
 import { CommonApiResponse } from "../../../types/general";
 
-type Payload = {};
+type Payload = undefined;
 
 connect();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<CommonApiResponse<Payload>>
+  res: NextApiResponse<CommonApiResponse<Payload> | null>
 ) {
   try {
     if (!req.method || req.method! !== "POST") {
-      res.status(405);
+      res.status(405).send(null);
       return;
     }
 
     req.log.info({ payload: req.body }, "Incoming Webhook Payload");
     await handleWebhook(req.body);
 
-    res.status(200).json({ status: "ok", data: {} });
+    res.status(200).json({ status: "ok", data: undefined });
   } catch (err) {
     errorResponse(req, res, err);
   }
