74171d2b43d9dfc5592003d9b1070ddec554bc1c
Talerchik-Aleksey <talerchik@protonmail.com>
2023-02-27 09:39:29 +0300

    Add meta tags and title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Connect with like-minded individuals who share your values of freedom and liberty. LibertyMeets is the perfect platform for finding new friends in your town."
+        />
+        <meta
+          property="og:title"
+          content="LibertyMeets - Find Freedom-Friendly Folks Near You"
+        />
+        <meta
+          property="og:description"
+          content="Connect with like-minded individuals who share your values of freedom and liberty. LibertyMeets is the perfect platform for finding new friends in your town."
+        />
+        <meta
+          name="twitter:title"
+          content="LibertyMeets - Find Freedom-Friendly Folks Near You"
+        />
+        <meta
+          name="twitter:description"
+          content="Connect with like-minded individuals who share your values of freedom and liberty. LibertyMeets is the perfect platform for finding new friends in your town."
+        />
+        <meta property="og:image" content="/favicon.png" />
+        <meta property="twitter:image" content="/favicon.png" />
         <link rel="icon" type="image/svg" href="/favicon.svg" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" />
